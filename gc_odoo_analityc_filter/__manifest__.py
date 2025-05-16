2801901b0ed6f8fd6fcbb4bb1c997224baa9e851
pjabie <pjabie@broobe.com>
2025-05-16 10:23:43 -0500

    Se añade filtro para etiquetas analiticas

---
+++ gc_odoo_analityc_filter/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Filtro segun cuentas analiticas en facturas',
+    'version': '16.0.1.0.1',
+    'summary': 'Permite filtrar las cuentas analiticas en las facturas',
+    'author': 'GauchoCode',
+    'depends': ['account'],
+    'data': [
+        'views/account_move_views.xml',
+    ],
+    'installable': True,
+    'license': 'AGPL-3',
+}
---
+++ gc_odoo_analityc_filter/models/account_move.py
@@ -0,0 +1,27 @@
+from odoo import models, fields, api, _
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    analytic_account_ids = fields.Many2many(
+        "account.analytic.account",
+        "account_move_analytic_account_rel",
+        "move_id",
+        "tag_id",
+        string="Analytic Tags",
+        store=True,
+        readonly=True,
+        copy=False,
+        compute="_compute_analytic_tag_ids",
+    )
+
+    @api.depends('invoice_line_ids.analytic_account_ids')
+    def _compute_analytic_tag_ids(self):
+        for rec in self:
+            rec.analytic_account_ids = rec.invoice_line_ids.mapped('analytic_account_ids')
+
+    @api.onchange("invoice_line_ids")
+    def _onchange_invoice_line_ids(self):
+        res = super(AccountMove, self)._onchange_invoice_line_ids()
+        self._compute_analytic_tag_ids()
+        return res
