8ec8a0713fa554be93da1a7c6c8a578e75b679bb
pjabie <pjabie@gauchocode.com>
2025-06-02 17:45:10 -0300

    Update __manifest__.py

--- gc_odoo_journal_email_template/__manifest__.py
+++ gc_odoo_journal_email_template/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Template de Correo Electrónico segun diario',
-    'version': '16.0.1.0.2',
+    'version': '16.0.1.0.3',
     'summary': 'Permite definir un template de correo electrónico por diario',
     'author': 'GauchoCode',
     'depends': ['account','account_ux'],
