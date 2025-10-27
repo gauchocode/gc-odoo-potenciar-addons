d78fe98d9dcae70e5c4ef0df8be6d469bc41105f
pjabie <pjabie@broobe.com>
2025-10-27 13:50:31 -0500

    UPD

--- gc_odoo_analityc_filter/models/account_move.py
+++ gc_odoo_analityc_filter/models/account_move.py
@@ -21,7 +21,6 @@ class AccountMove(models.Model):
             rec.analytic_account_ids = rec.invoice_line_ids.mapped('analytic_account_ids')
 
     @api.onchange("invoice_line_ids")
-    def _onchange_invoice_line_ids(self):
-        res = super(AccountMove, self)._onchange_invoice_line_ids()
-        self._compute_analytic_tag_ids()
-        return res
+    def _onchange_invoice_line_ids_analytic_filter(self):
+        for rec in self:
+            rec._compute_analytic_tag_ids()
